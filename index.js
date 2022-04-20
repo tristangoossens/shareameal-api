5ea27a195ba87051e42e9e4a871d0bcdee48cc8e
Tristan Goossens <tt.goossens@student.avans.nl>
2022-04-20 17:14:10 +0200

    added server file

---
+++ index.js
@@ -0,0 +1,19 @@
+// Import environment variables for local development
+require('dotenv').config();
+
+const express = require('express');
+const router = require('./src/api');
+
+// Create instance of express server
+const app = express()
+
+// Configure app to use JSON
+app.use(express.json());
+
+// Assign main router to app
+app.use(router);
+
+// Setup server to listen on specified port
+app.listen(process.env.PORT, () => {
+    console.log(`shareameal-api listening on port ${process.env.PORT}`)
+})
