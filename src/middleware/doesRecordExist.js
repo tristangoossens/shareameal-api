d779193d73e6fa26626a3fdfd8c063f99669cd11
Tristan Goossens <tt.goossens@student.avans.nl>
2022-05-10 22:49:49 +0200

    update middlewares

--- src/middleware/doesRecordExist.js
+++ src/middleware/doesRecordExist.js
@@ -1,4 +1,4 @@
-const doesUserExist = (req, res, next) => {
+const doesUserWithIDExist = (req, res, next) => {
     db.query('SELECT COUNT(*) AS c FROM `user` WHERE `id` = ?', [req.params.id]).then((resp) => {
         const exists = Boolean(JSON.parse(JSON.stringify(resp[0][0])).c);
         if (exists) {
@@ -17,5 +17,27 @@ const doesUserExist = (req, res, next) => {
     });
 }
 
+const doesUserWithEmailExist = (req, res, next) => {
+    db.query('SELECT COUNT(*) AS c FROM `user` WHERE `emailAdress` = ?', [req.body.emailAdress]).then((resp) => {
+        const exists = Boolean(JSON.parse(JSON.stringify(resp[0][0])).c);
+        if (!exists) {
+            next();
+        } else {
+            res.status(400).send({
+                status: 400,
+                error: `Requested user with email '${req.body.emailAdress}' already exists`
+            });
+        }
+    }).catch((err) => {
+        res.status(500).send({
+            status: 500,
+            error: err.message
+        })
+    });
+}
 
-module.exports = {doesUserExist}
+
+module.exports = {
+    doesUserWithIDExist,
+    doesUserWithEmailExist,
+}
--- src/middleware/validateRequestBody.js
+++ src/middleware/validateRequestBody.js
@@ -2,17 +2,25 @@ const assert = require('assert');
 
 const userBody = (req, res, next) => {
     try {
-        const {firstname, lastname, street, city, password, email} = req.body;
+        const { firstName, lastName, street, city, password, emailAdress } = req.body;
 
-    
-        assert.equal(typeof firstname == 'string', 'Firstname must be in string format');
-        assert.equal(typeof lastname == 'string', 'Lastname must be in string format');
+
+        assert.equal(typeof firstName == 'string', 'Firstname must be in string format');
+        assert.equal(typeof lastName == 'string', 'Lastname must be in string format');
+        assert.equal(typeof street == 'string', 'Street must be in string format');
+        assert.equal(typeof city == 'string', 'City must be in string format');
+        assert.equal(typeof password == 'string', 'Password must be in string format');
+        assert.equal(typeof emailAdress == 'string', 'Email must be in string format');
 
         next()
-    } catch(err) {
-        res.status(403).send({
-            status: 403,
+    } catch (err) {
+        res.status(400).send({
+            status: 400,
             error: err.message
         })
     }
+}
+
+module.exports = {
+    userBody
 }
