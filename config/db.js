e135dd5226f27560a877fb75ed4a4e4dbd1e7605
Tristan Goossens <tt.goossens@student.avans.nl>
2022-05-11 15:56:48 +0200

    allow multiple statements in pool

--- config/db.js
+++ config/db.js
@@ -7,6 +7,7 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
+    multipleStatements: true,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
