7d2ac7fec5533813bb77b08e7dfc1815efcf1af1
Tristan Goossens <tt.goossens@student.avans.nl>
2022-05-21 16:26:43 +0200

    implement remaining tests for auth routes

--- test/auth.test.js
+++ test/auth.test.js
@@ -11,14 +11,14 @@ chai.use(chaiHttp)
 const clearQuery = 'DELETE IGNORE FROM `meal`; DELETE IGNORE FROM `meal_participants_user`; DELETE IGNORE FROM `user`; ALTER TABLE `user` AUTO_INCREMENT = 1;';
 const dummyUserQuery = (email) => {
     return `INSERT INTO user (id, firstName, lastName, emailAdress, password, street, city)
-    VALUES (0, "Test", "Tester", "${email}", "mooi123", "Teststraat 14", "Pietstad");`
+    VALUES (0, "Test", "Tester", "${email}", "Test1234", "Teststraat 14", "Pietstad");`
 }
 
 describe('ShareAMeal Auth routes', () => {
     describe('UC-101 Login to existing user', () => {
         const userLoginDetails = {
             emailAdress: 'test@mail.com',
-            password: 'mooi123'
+            password: 'Test1234'
         }
 
         before((done) => {
@@ -64,6 +64,56 @@ describe('ShareAMeal Auth routes', () => {
 
                 // Check if the error message is correct
                 message.should.be.an('string').that.contains('Password must be in string format');
+
+                done()
+            })
+        })
+
+        it('TC-101-2 should return error when a non-valid email is passed in', (done) => {
+            // Clone dummy object
+            const login = Object.assign({}, userLoginDetails);
+
+            // Set invalid email address
+            login.emailAdress = 'test@mail';
+
+            chai.request(server).post('/api/auth/login').send(login).end((err, res) => {
+                assert.ifError(err)
+
+                // Check if a 400 status code was returned with an error message
+                res.should.have.status(400)
+                res.should.be.an('object')
+                res.body.should.be.an('object').that.has.all.keys('message');
+
+                // Disect response object
+                let { message } = res.body
+
+                // Check if the error message is correct
+                message.should.be.an('string').that.contains('Email must be in valid e-mail format');
+
+                done()
+            })
+        })
+
+        it('TC-101-2 should return error when a non-valid password is passed in', (done) => {
+            // Clone dummy object
+            const login = Object.assign({}, userLoginDetails);
+
+            // Set invalid email address
+            login.password = 'password'; // No number in password
+
+            chai.request(server).post('/api/auth/login').send(login).end((err, res) => {
+                assert.ifError(err)
+
+                // Check if a 400 status code was returned with an error message
+                res.should.have.status(400)
+                res.should.be.an('object')
+                res.body.should.be.an('object').that.has.all.keys('message');
+
+                // Disect response object
+                let { message } = res.body
+
+                // Check if the error message is correct
+                message.should.be.an('string').that.contains('Password must be 8 characters long and contain a number and a letter');
 
                 done()
             })
